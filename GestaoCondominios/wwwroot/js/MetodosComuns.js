75ab147429b07d709354508f4e586628a9502558
danfrgo <danielfr_goncalves@hotmail.com>
2020-12-05 17:35:36 +0000

    Dashboard (2)

---
+++ GestaoCondominios/wwwroot/js/MetodosComuns.js
@@ -0,0 +1,25 @@
+﻿function ObterMeses(dados) {
+    var labels = [];
+    var tamanho = dados.length;
+    var indice = 0; // para percorrer a lista
+
+    while (indice < tamanho) {
+        labels.push(dados[indice].meses);
+        indice = indice + 1;
+    }
+
+    return labels;
+}
+
+function ObterValores(dados) {
+    var valores = [];
+    var tamanho = dados.length;
+    var indice = 0;
+
+    while (indice < tamanho) {
+        valores.push(dados[indice].valores);
+        indice = indice + 1;
+    }
+
+    return valores;
+}
